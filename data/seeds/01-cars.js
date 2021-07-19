4a99ebd44f8781ac3dca39505814dd2506d6f273
Nora C <nocorser@gmail.com>
2021-07-19 11:57:47 -0500

    setup, dependencies, seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,40 @@
 // STRETCH
+/**
+vin     string	required, unique
+make	string	required
+model	string	required
+mileage	numeric	required
+title	string	optional
+transmission	string	optional
+ */
+
+const cars = [
+    {
+        vin: '1234567901234567',
+        make: 'toyota',
+        model: 'prius',
+        mileage: '525600',
+        title: 'clean',
+        transmission: 'auto'
+    },
+    {
+        vin: '1234567901234568',
+        make: 'honda',
+        model: 'civic',
+        mileage: '192837192847',
+        title: 'hand-me down',
+        transmission: 'auto'
+    },
+    {
+        vin: '1234567901234569',
+        make: 'toyota',
+        model: 'tacoma',
+        mileage: '10',
+        title: 'compromising',
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
